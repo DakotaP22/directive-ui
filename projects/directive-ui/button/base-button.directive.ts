203d992b02c5e39dd53150db26f66f9c5524edc2
Dakota Pease <peased22@gmail.com>
2025-04-22 00:00:19 -0400

    feat: Adds test for base-button directive to control general button theming

---
+++ projects/directive-ui/button/base-button.directive.ts
@@ -0,0 +1,22 @@
+import { Directive, effect, HostBinding, HostListener, input } from "@angular/core";
+
+export type ButtonColor = 'primary' | 'secondary' | 'danger' | 'success' | 'cancel';
+
+@Directive({
+    standalone: true,
+})
+export class BaseButtonDirective {
+
+    @HostBinding('class') className: string = 'dui-button';
+
+    @HostBinding('style.background-color') bgColor = "var(--dui-button-bg-primary-default)";
+    @HostListener('onmouseenter') onMouseEnter = () => this.bgColor = `var(--dui-button-bg-${this.color()}-hover)`;
+    @HostListener('onmouseleave') onMouseLeave = () => this.bgColor = `var(--dui-button-bg-${this.color()}-default)`;
+
+
+    color = input<ButtonColor>('primary');
+    _ = effect(() => {
+        console.log(this.color());
+        this.bgColor = `var(--dui-button-bg-${this.color()}-default)`;
+    })
+}
