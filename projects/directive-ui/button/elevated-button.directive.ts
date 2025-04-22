052c625c1bc3e1ee17023ffa178b2311dd04168e
Dakota Pease <peased22@gmail.com>
2025-04-22 00:00:37 -0400

    feat: Adds test for elevated button directive

--- projects/directive-ui/button/elevated-button.directive.ts
+++ projects/directive-ui/button/elevated-button.directive.ts
@@ -1,16 +1,29 @@
-import { style } from "@angular/animations";
-import { Directive, HostBinding, input } from "@angular/core";
+import { Directive, HostBinding } from "@angular/core";
+import { SurfaceDirective } from "../elevation/surface.directive";
+import { BaseButtonDirective } from "./base-button.directive";
+
 
 @Directive({
-    selector: "button[dui-elevated-button]"
+    standalone: true,
+    selector: "button[dui-elevated-button]",
+    hostDirectives: [
+        {
+            directive: SurfaceDirective,
+            inputs: ["level"]
+        },
+        {
+            directive: BaseButtonDirective,
+            inputs: ["color"]
+        }
+    ],
 })
 export class ElevatedButtonDirective {
-
     @HostBinding("style") style = {
-        "background-color": "black",
-        "color": "white",
-        "border": "10px solid blue",
+        "font-size": "var(--dui-elevated-button-font-size, 1rem)",
+        "padding-inline": "var(--dui-elevated-button-padding-inline, 1rem)",
+        "padding-block": "var(--dui-elevated-button-padding-block, 0.5rem)",
+        "border-radius": "var(--dui-elevated-button-border-radius, 0.35rem)",
+        "border-color": "var(--dui-elevated-button-border-color, black)",
+        "border-width": "var(--dui-elevated-button-border-width, 1px)",
     }
-
-    test = input<string>();
 }
---
+++ projects/directive-ui/button/elevated-button.stories.ts
@@ -0,0 +1,52 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+import { fn } from '@storybook/test';
+import { ElevatedButtonDirective } from './elevated-button.directive';
+import { SurfaceDirective } from '../elevation/surface.directive';
+import { BaseButtonDirective } from './base-button.directive';
+
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories
+const meta: Meta<ElevatedButtonDirective & SurfaceDirective & BaseButtonDirective> = {
+  title: 'Elevated Button',
+  component: ElevatedButtonDirective,
+  tags: ['autodocs'],
+  argTypes: {
+    level: {
+      description: 'The elevation level of the button',
+      table: {
+        type: { summary: '1 | 2 | 3 | 4 | 5' },
+        defaultValue: { summary: '1' }
+      },
+      options: [1, 2, 3, 4, 5],
+      control: {
+        type: 'select',
+      }
+    },
+    color: {
+      description: 'The color class to use for the button',
+      table: {
+        type: { summary: 'primary | secondary' },
+        defaultValue: { summary: 'primary' }
+      },
+      options: ['primary', 'secondary'],
+      control: {
+        type: 'select',
+      },
+    }
+  },
+  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+  args: {
+    level: 1,
+  },
+  render: (args) => ({
+    props: args,
+    template: `<button dui-elevated-button [level]="level" [color]="color">Test</button>`,
+  })
+};
+
+export default meta;
+type Story = StoryObj<ElevatedButtonDirective & SurfaceDirective & BaseButtonDirective>;
+
+export const Primary: Story = {
+ tags: ['!dev']
+}
