fa820d70a8c3679835d601624d0a591a9bf841e8
Dakota Pease <peased22@gmail.com>
2025-04-23 19:39:20 -0400

    fix: Fixes issues with mouseenter and mouseleave not firing

--- projects/directive-ui/button/base-button.directive.ts
+++ projects/directive-ui/button/base-button.directive.ts
@@ -8,18 +8,49 @@ export type ButtonColor = 'primary' | 'secondary' | 'danger' | 'success' | 'canc
 export class BaseButtonDirective {
 
     @HostBinding('class') className: string = 'dui-button';
-
+    @HostBinding('style.cursor') cursor = 'pointer';
     @HostBinding('style.background-color') bgColor = "var(--dui-button-bg-primary-default)";
-    @HostListener('onmouseenter') onMouseEnter = () => this.bgColor = `var(--dui-button-bg-${this.color()}-hover)`;
-    @HostListener('onmouseleave') onMouseLeave = () => this.bgColor = `var(--dui-button-bg-${this.color()}-default)`;
-    
     @HostBinding('style.color') fontColor = "var(--dui-button-text-primary-default)";
+    @HostBinding('style.outline') outline = 'none';
+
+    @HostListener('mouseenter') mouseEnter = () => this.onMouseEnter();
+    @HostListener('mouseleave') mouseLeave = () => this.onMouseLeave();
+    @HostListener('mousedown') mouseDown = () => this.onMouseDown();
+    @HostListener('mouseup') mouseUp = () => this.onMouseUp();
+    @HostListener('focus') focus = () => this.onFocus();
+    @HostListener('blur') blur = () => this.onBlur();
+
+    
 
 
     color = input<ButtonColor>('primary');
     _ = effect(() => {
-        console.log(this.color());
         this.bgColor = `var(--dui-button-bg-${this.color()}-default)`;
         this.fontColor = `var(--dui-button-text-${this.color()}-default)`;
     })
+
+    onMouseEnter() {
+        this.bgColor = `var(--dui-button-bg-${this.color()}-hover)`;
+    }
+
+    onMouseLeave() {
+        this.bgColor = `var(--dui-button-bg-${this.color()}-default)`;
+    }
+
+    onMouseDown() {
+        this.bgColor = `var(--dui-button-bg-${this.color()}-active)`;
+    }
+
+    onMouseUp() {
+        this.bgColor = `var(--dui-button-bg-${this.color()}-default)`;
+    }
+
+    onFocus() {
+        this.bgColor = `var(--dui-button-bg-${this.color()}-focused)`;
+    }
+
+    onBlur() {
+        this.bgColor = `var(--dui-button-bg-${this.color()}-default)`;
+    }
+
 }
