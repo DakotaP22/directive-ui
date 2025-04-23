17ab9a5b3b4ebb018d033c69a6b98b850582c9f2
Dakota Pease <peased22@gmail.com>
2025-04-22 23:11:36 -0400

    feat: Adds support for font color

--- projects/directive-ui/button/base-button.directive.ts
+++ projects/directive-ui/button/base-button.directive.ts
@@ -12,11 +12,14 @@ export class BaseButtonDirective {
     @HostBinding('style.background-color') bgColor = "var(--dui-button-bg-primary-default)";
     @HostListener('onmouseenter') onMouseEnter = () => this.bgColor = `var(--dui-button-bg-${this.color()}-hover)`;
     @HostListener('onmouseleave') onMouseLeave = () => this.bgColor = `var(--dui-button-bg-${this.color()}-default)`;
+    
+    @HostBinding('style.color') fontColor = "var(--dui-button-text-primary-default)";
 
 
     color = input<ButtonColor>('primary');
     _ = effect(() => {
         console.log(this.color());
         this.bgColor = `var(--dui-button-bg-${this.color()}-default)`;
+        this.fontColor = `var(--dui-button-text-${this.color()}-default)`;
     })
 }
