491db5a04f8821cd633acd50abf83507ee4eee5d
Dakota Pease <peased22@gmail.com>
2025-04-26 15:42:04 -0400

    feat: Introduces a function and mixin to generate color palletes

--- projects/directive-ui/.storybook/main.ts
+++ projects/directive-ui/.storybook/main.ts
@@ -9,7 +9,6 @@ const config: StorybookConfig = {
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
-    "@storybook/addon-styling-webpack"
   ],
   "framework": {
     "name": "@storybook/angular",
