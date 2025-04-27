50d73f035607dcfffb729e3889bf250c5211dadb
Dakota Pease <peased22@gmail.com>
2025-04-26 23:03:49 -0400

    refactor: Update badge stories to include example with outlined button

--- projects/directive-ui/badge/badge.stories.ts
+++ projects/directive-ui/badge/badge.stories.ts
@@ -1,11 +1,17 @@
-import type { Meta, StoryObj } from '@storybook/angular';
+import { moduleMetadata, type Meta, type StoryObj } from '@storybook/angular';
 import { BadgeDirective } from './badge.directive';
+import { OutlineButtonDirective } from '../button/outline-button.directive';
 
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
 const meta: Meta<BadgeDirective> = {
-  title: 'Badge',
+  title: 'Badges / Badge',
   component: BadgeDirective,
+  decorators: [
+    moduleMetadata({
+      imports: [ OutlineButtonDirective ]
+    })
+  ],
   tags: ['autodocs'],
   argTypes: {
     size: {
@@ -45,8 +51,19 @@ const meta: Meta<BadgeDirective> = {
           dui-badge 
           [size]="size" 
           [color]="color"
-        >9999+</span>
+        >99</span>
       </a>
+
+      <br />
+
+      <button dui-outline-button>
+        Emptry Trash
+        <span 
+          dui-badge 
+          [size]="size" 
+          [color]="color"
+        >33</span>
+      </button>
       `,
   })
 };
