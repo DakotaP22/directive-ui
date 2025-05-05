2cab4a243fcb560b054dba297ffbd91e72a636c8
Dakota Pease <peased22@gmail.com>
2025-05-05 19:38:54 -0400

    feat: Adds align input to the card-action direction

--- projects/directive-ui/card/card-actions.directive.ts
+++ projects/directive-ui/card/card-actions.directive.ts
@@ -1,8 +1,14 @@
-import { Directive, HostBinding } from "@angular/core";
+import { Directive, effect, HostBinding, input } from "@angular/core";
 
 @Directive({
     selector: 'div[dui-card-actions]'
 })
 export class CardActionsDirective {
     @HostBinding('class.dui-card-actions') duiCardActions = true;
+    @HostBinding('style.justify-content') justifyContentStyle = 'right';
+
+    align = input<'left' | 'right' | 'center'>('right');
+    _ = effect(() => {
+        this.justifyContentStyle = this.align();
+    })
 }
